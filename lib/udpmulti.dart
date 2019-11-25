165788f0f67e2c4c84752e8e285ef3c7f9ee75ae
imdhanush <dhanushdazz@gmail.com>
2019-11-25 23:40:45 +0530

    Defined Udp class

--- lib/udpmulti.dart
+++ lib/udpmulti.dart
@@ -1,5 +1,39 @@
 import 'dart:io';
 
-Future<String> udpMultiCast ()async{
+abstract class Udp {
+  InternetAddress get address;
 
+  int get port;
+
+  Future<RawDatagramSocket> bind();
+}
+
+class UDPTester extends Udp {
+  int multicastPort;
+  InternetAddress multicastAddress;
+
+  UDPTester() {
+    multicastPort = 3333;
+    multicastAddress = new InternetAddress('224.9.9.9');
+  }
+
+  UDPTester.initialize(String addr, int port) {
+    this.multicastPort = port;
+    this.multicastAddress = new InternetAddress(addr);
+  }
+
+  UDPTester.setMulticastPort(this.multicastPort);
+
+  UDPTester.setMulticastAddress(this.multicastAddress);
+
+  @override
+  InternetAddress get address => multicastAddress;
+
+  @override
+  int get port => multicastPort;
+
+  @override
+  Future<RawDatagramSocket> bind() {
+    return RawDatagramSocket.bind(this.multicastAddress, this.multicastPort);
+  }
 }
